ead0302a61d189c760eeb7d07b397e821ef75d73
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2018-06-18 08:01:06 +0800

    Added update notice

--- includes/settings-billplz.php
+++ includes/settings-billplz.php
@@ -5,104 +5,87 @@ if (!defined('ABSPATH')) {
 }
 
 /**
- * Settings for Billplz for WooCommerce.
+ * Settings for PayPal Gateway.
  */
 return array(
     'enabled' => array(
-        'title' => __('Enable/Disable', 'bfw'),
+        'title' => __('Enable/Disable', 'wcbillplz'),
         'type' => 'checkbox',
-        'label' => __('Enable Billplz', 'bfw'),
+        'label' => __('Enable Billplz', 'wcbillplz'),
         'default' => 'yes'
     ),
     'title' => array(
-        'title' => __('Title', 'bfw'),
+        'title' => __('Title', 'wcbillplz'),
         'type' => 'text',
-        'description' => __('This controls the title which the user sees during checkout.', 'bfw'),
-        'default' => __('Billplz Payment Gateway', 'bfw')
+        'description' => __('This controls the title which the user sees during checkout.', 'wcbillplz'),
+        'default' => __('Billplz Payment Gateway', 'wcbillplz')
     ),
     'description' => array(
-        'title' => __('Description', 'bfw'),
+        'title' => __('Description', 'wcbillplz'),
         'type' => 'textarea',
-        'description' => __('This controls the description which the user sees during checkout.', 'bfw'),
-        'default' => __('Pay with <strong>Maybank2u, CIMB Clicks, Bank Islam, RHB, Hong Leong Bank, Bank Muamalat, Public Bank, Alliance Bank, Affin Bank, AmBank, Bank Rakyat, UOB, Standard Chartered </strong>. ', 'bfw')
+        'description' => __('This controls the description which the user sees during checkout.', 'wcbillplz'),
+        'default' => __('Pay with <strong>Maybank2u, CIMB Clicks, Bank Islam, RHB, Hong Leong Bank, Bank Muamalat, Public Bank, Alliance Bank, Affin Bank, AmBank, Bank Rakyat, UOB, Standard Chartered </strong>. ', 'wcbillplz')
     ),
     'api_key' => array(
-        'title' => __('API Secret Key', 'bfw'),
+        'title' => __('API Secret Key', 'wcbillplz'),
         'type' => 'text',
         'placeholder' => 'Example : ed586547-00b7-459a-a02e-7e876a744590',
-        'description' => __('Please enter your Billplz Api Key.', 'bfw') . ' ' . sprintf(__('Get Your API Key: %sBillplz%s.', 'bfw'), '<a href="https://www.billplz.com/enterprise/setting" target="_blank">', '</a>'),
+        'description' => __('Please enter your Billplz Api Key.', 'wcbillplz') . ' ' . sprintf(__('Get Your API Key: %sBillplz%s.', 'wcbillplz'), '<a href="https://www.billplz.com/enterprise/setting" target="_blank">', '</a>'),
         'default' => ''
     ),
     'collection_id' => array(
-        'title' => __('Collection ID', 'bfw'),
+        'title' => __('Collection ID', 'wcbillplz'),
         'type' => 'text',
         'placeholder' => 'Example : ugo_7dit',
-        'description' => __('Please enter your Billplz Collection ID. ', 'bfw') . ' ' . sprintf(__('Login to Billplz >> Billing >> Create Collection. %sLink%s.', 'bfw'), '<a href="https://www.billplz.com/enterprise/billing" target="_blank">', '</a>'),
+        'description' => __('Please enter your Billplz Collection ID. ', 'wcbillplz') . ' ' . sprintf(__('Login to Billplz >> Billing >> Create Collection. %sLink%s.', 'wcbillplz'), '<a href="https://www.billplz.com/enterprise/billing" target="_blank">', '</a>'),
         'default' => ''
     ),
     'x_signature' => array(
-        'title' => __('X Signature Key', 'bfw'),
+        'title' => __('X Signature Key', 'wcbillplz'),
         'type' => 'text',
         'placeholder' => 'Example : S-0Sq67GFD9Y5iXmi5iXMKsA',
-        'description' => __('Please enter your Billplz X Signature Key. ', 'bfw') . ' ' . sprintf(__('Login to Billplz >> Settings >> Enable X Signature. %sLink%s.', 'bfw'), '<a href="https://www.billplz.com/enterprise/billing" target="_blank">', '</a>'),
+        'description' => __('Please enter your Billplz X Signature Key. ', 'wcbillplz') . ' ' . sprintf(__('Login to Billplz >> Settings >> Enable X Signature. %sLink%s.', 'wcbillplz'), '<a href="https://www.billplz.com/enterprise/billing" target="_blank">', '</a>'),
         'default' => ''
     ),
     'notification' => array(
-        'title' => __('Bill Notification', 'bfw'),
+        'title' => __('Bill Notification', 'wcbillplz'),
         'type' => 'select',
         'class' => 'wc-enhanced-select',
-        'description' => __('No Notification - Customer will NOT receive any notification. Email Only - Customer will receive Email Notification for payment. SMS Only - Customer will receive SMS Notification for payment. Both - Customer will receive Email & SMS Notification for payment.', 'bfw'),
+        'description' => __('No Notification - Customer will NOT receive any notification. Email Only - Customer will receive Email Notification for payment. SMS Only - Customer will receive SMS Notification for payment. Both - Customer will receive Email & SMS Notification for payment.', 'wcbillplz'),
         'default' => 'None',
         'desc_tip' => true,
         'options' => array(
-            '0' => __('No Notification (Recommended)', 'bfw'),
-            '1' => __('Email Only (FREE)', 'bfw'),
-            '2' => __('SMS Only (RM0.15)', 'bfw'),
-            '3' => __('Both (RM0.15)', 'bfw')
+            '0' => __('No Notification', 'wcbillplz'),
+            '1' => __('Email Only (FREE)', 'wcbillplz'),
+            '2' => __('SMS Only (RM0.15)', 'wcbillplz'),
+            '3' => __('Both (RM0.15)', 'wcbillplz')
         )
     ),
     'clearcart' => array(
-        'title' => __('Clear Cart Session', 'bfw'),
+        'title' => __('Clear Cart Session', 'wcbillplz'),
         'type' => 'checkbox',
-        'label' => __('Tick to clear cart session on checkout', 'bfw'),
+        'label' => __('Tick to clear cart session on checkout', 'wcbillplz'),
         'default' => 'no'
     ),
     'debug' => array(
-        'title' => __('Debug Log', 'bfw'),
+        'title' => __('Debug Log', 'wcbillplz'),
         'type' => 'checkbox',
-        'label' => __('Enable logging', 'bfw'),
+        'label' => __('Enable logging', 'wcbillplz'),
         'default' => 'no',
-        'description' => sprintf(__('Log Billplz events, such as IPN requests, inside <code>%s</code>', 'bfw'), wc_get_log_file_path('billplz'))
+        'description' => sprintf(__('Log Billplz events, such as IPN requests, inside <code>%s</code>', 'wcbillplz'), wc_get_log_file_path('billplz'))
     ),
     'instructions' => array(
-        'title' => __('Instructions', 'bfw'),
+        'title' => __('Instructions', 'wcbillplz'),
         'type' => 'textarea',
-        'description' => __('Instructions that will be added to the thank you page and emails.', 'bfw'),
+        'description' => __('Instructions that will be added to the thank you page and emails.', 'wcbillplz'),
         'default' => '',
         'desc_tip' => true,
     ),
     'custom_error' => array(
-        'title' => __('Error Message', 'bfw'),
+        'title' => __('Error Message', 'wcbillplz'),
         'type' => 'text',
         'placeholder' => 'Example : You have cancelled the payment. Please make a payment!',
-        'description' => __('Error message that will appear when customer cancel the payment.', 'bfw'),
+        'description' => __('Error message that will appear when customer cancel the payment.', 'wcbillplz'),
         'default' => 'You have cancelled the payment. Please make a payment!'
-    ),
-    'reference_1_label' => array(
-        'title' => __('Reference 1 Label', 'bfw'),
-        'type' => 'text',
-        'default' => ''
-    ),
-    'reference_1' => array(
-        'title' => __('Reference 1', 'bfw'),
-        'type' => 'text',
-        'default' => ''
-    ),
-    'checkout_label' => array(
-        'title' => __('Checkout Label', 'bfw'),
-        'type' => 'text',
-        'placeholder' => 'Example: Pay with Billplz',
-        'description' => __('Button label on checkout.', 'bfw'),
-        'default' => 'Pay with Billplz'
     )
 );
--- readme.txt
+++ readme.txt
@@ -2,23 +2,25 @@
 Contributors: wanzulnet
 Tags: billplz,paymentgateway,fpx,malaysia
 Tested up to: 4.9.6
-Stable tag: 3.18
+Stable tag: 3.19.0
 Donate link: http://billplz.com/join/lz7pmrxa45tiihvqdydxqq/
-Requires at least: 4.6
-License: GPL-3.0-or-later
+Requires at least: 4.3
+License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Requires PHP: 5.6
 
-Accept Internet Banking Payment by using Billplz.
+Accept Internet Banking Payment by using Billplz. 
 
 == Description ==
-Install this plugin to accept payment using Billplz (Maybank2u, CIMB Clicks, Bank Islam, FPX).
+Install this plugin to accept payment using Billplz (Maybank2u, CIMB Clicks, Bank Islam, FPX). 
 
-== Upgrade Notice ==
+== Upgrade Notice == 
 
-WARNING! THIS UPDATE WILL BREAK YOUR SITE!
+WARNING! THIS UPDATE MAY BREAK YOUR SITE!
 
-Please re-configure the plugin if you upgrading from version 3.19.0 or earlier
+Please re-configure the plugin if you upgrading from version 3.14 or ealier
+1. Make sure your PHP Version is : 5.6/7.0/7.1
+2. Set X Signature Key
 
 == Screenshots ==
 * Installing Billplz for WooCommerce
@@ -28,19 +30,13 @@ Please re-configure the plugin if you upgrading from version 3.19.0 or earlier
 
 == Changelog ==
 
-= 3.20.1 =
-* FIX: Floating number precision issue.
+= 3.19.0 =
 
-= 3.20.0 =
+* NEW: Added warning for update to the next major release 3.20.x
 
-* NEW: Introduced semantic versioning. X.Y.Z (X: API Version, Y: Major Release, Z: Minor Release)
-* NEW: Ability to customize Proceed to Checkout button label
-* NEW: Ability to add more information on Bills using Reference 2
-* NEW: Ability to re-query bill and order for payment status
-* IMPROVED: Order is now strongly coupled with Bill
-* IMPROVED: CPU usage spike by previous version caused by Cron Jobs
-* IMPROVED: Filters and Action Hooks are introduced
-* IMPROVED: Support for using composer
+= 3.18 =
+
+* IMPROVED: Bills will not be created twice in some circumstances
 
 == Installation ==
 
@@ -76,10 +72,10 @@ You can the X Signature Key at your Billplz Account Settings. [Get it here](http
 1. If you are not getting a **Callback/Redirect** response from Billplz:
 
 	Please make sure you have **Tick "Enable XSignature Payment Completion"** on Billplz Account Settings and make sure you have set your **X Signature Key**.
-
+	
 2. If you want both Email & Phone Number to be captured on Bills:
 
-	Set Notification settings to **No Notification** or **Both**
+	Set Notification settings to **No Notification** or **Both**	
 
 == Links ==
 [Sign Up](http://billplz.com/join/lz7pmrxa45tiihvqdydxqq/) for Billplz account to accept payment using Billplz now!
